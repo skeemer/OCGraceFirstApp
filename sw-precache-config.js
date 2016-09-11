47c32e026779790e2d75bfe9263cb854345438e8
Leo Lutz <leo@leolutz.com>
2016-09-11 16:10:08 -0700

    Get the service worker and app-storage working in chrome and safari.

--- sw-precache-config.js
+++ sw-precache-config.js
@@ -1,8 +1,23 @@
 module.exports = {
+  runtimeCaching: [
+    {
+      urlPattern: new RegExp('/bower_components/app-storage/app-indexeddb-mirror/common-worker-scope.js\\?.*'),
+      handler: 'networkFirst'
+    },
+    {
+      urlPattern: new RegExp('/images/app-icon-.*'),
+      handler: 'cacheFirst'
+    }
+  ],
   staticFileGlobs: [
     '/index.html',
     '/manifest.json',
-    '/bower_components/webcomponentsjs/webcomponents-lite.min.js'
+    '/bower_components/webcomponentsjs/webcomponents-lite.min.js',
+    '/bower_components/app-storage/app-indexeddb-mirror/app-indexeddb-mirror-worker.js'
   ],
   navigateFallback: '/index.html'
 };
+
+function escape_slash(path) {
+  return new RegExp(path);
+}
